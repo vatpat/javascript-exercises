691b67217e52c4ffa62a7cf96a88293579e9ebca
Vatsal <vatrpat@gmail.com>
2023-04-13 03:30:15 -0500

    Started JS Exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, count) {
+    if(count < 0) {
+        return "ERROR";
+    }
+    let repeated = "";
+    for(let i = 0; i < count; i++) {
+        repeated += str;
+    }
+    return repeated;
 };
 
 // Do not edit below this line
